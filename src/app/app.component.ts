5f8649f023966e4251aeaa62bbe995019f0fa162
lalit <lalitpatil80@gmail.com>
2020-02-22 23:42:21 +0530

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { log } from 'util';
-import {  }
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +9,26 @@ import {  }
 })
 export class AppComponent {
   title = 'DropDown Task';
-  IsMake:boolean = true;
-  IsModel:boolean = true;
+  // IsMake:boolean = true;
+  // IsModel:boolean = true;
   arrYear:any=[2010,2010,2009,2010];
   arrMake:any = ['Audi','Audi','BMW','BMW'];
   arrModel:any = ['A4','A8','X3','X6'];
   newYearArr:any;
   newMakeArr:any;
-  constructor(){
-    
-  }
+  dropdownForm:FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
   ngOnInit() {
+    this.dropdownForm = this.formBuilder.group({
+      year: ['', Validators.required],
+      make: ['', Validators.required],
+      model: ['', Validators.required]
+  });
     // this function call for unique array Element
     this.fnGetUniqueYearData();
     this.fnGetUniqueMakeData();
+    this.dropdownForm.controls['make'].disable();
+    this.dropdownForm.controls['model'].disable();
   };
   fnGetUniqueYearData(){
     this.newYearArr = new Set();
@@ -42,23 +48,37 @@ export class AppComponent {
     let selectedYear = e.target.value;
     console.log(selectedYear, 'selectedYear');
     if(selectedYear!=''){
-      this.IsMake = false;
+      this.dropdownForm.controls['make'].enable();
     }else{
-      this.IsMake = true;
-      this.IsModel = true;
+      this.dropdownForm.patchValue({
+        model:"",
+        make:""
+      });
+      this.dropdownForm.controls['make'].disable();
+      this.dropdownForm.controls['model'].disable();
     }
   }
   fnSelectedMake(e){
     let selectedMake = e.target.value;
     console.log(selectedMake, 'selectedMake');
     if(selectedMake!=''){
-      this.IsModel = false;
+      this.dropdownForm.controls['model'].enable();
     }else{
-      this.IsModel = true;
+      this.dropdownForm.patchValue({
+        model:""
+      });
+      this.dropdownForm.controls['model'].disable();
     }
   }
   fnSelectedModel(e){
     let selectedModel = e.target.value;
     console.log(selectedModel, 'selectedModel');
+    }
+    onSubmit() {
+      console.warn(this.dropdownForm.value);
+      alert(
+        "You selected values are: \nYear: "+ this.dropdownForm.value.year+
+        "\n make: "+this.dropdownForm.value.make +" \n Model: "+this.dropdownForm.value.model
+      );
     }
 }
